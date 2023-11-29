d84760081dd91582a57314aea644c3b8a0b1a6c9
Elvis Ndubuisi Ike <provictor.ie@gmail.com>
2023-11-29 17:26:53 +0100

    chore: added project configs

---
+++ src/configs/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "Barlink",
+  description: "barlink description",
+  url: "https://barlink.vercel.app",
+  ogImage: "https://path-to-image",
+  author: {
+    name: "Elvis Ndubuisi Ike",
+    twitter: "https://twitter.come/_elvis_ndubuisi",
+    github: "https://github.com/elvis_ndubuisi",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
---
+++ src/env.mjs
@@ -0,0 +1,15 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.string().url(),
+  },
+  client: {
+    NEXT_PUBLIC_GOOGLE_PUBLIC_ID: z.string(),
+  },
+  runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXT_PUBLIC_GOOGLE_ID: process.env.NEXT_PUBLIC_GOOGLE_ID,
+  },
+});
