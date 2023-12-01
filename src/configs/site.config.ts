737bfe772466430e798cc79c5bf3f283323fc4f4
Elvis Ndubuisi Ike <provictor.ie@gmail.com>
2023-12-01 14:39:33 +0100

    chore: renamed site config file

--- src/configs/site.config.ts
+++ src/configs/site.config.ts
@@ -7,6 +7,9 @@ export const siteConfig = {
     name: "Elvis Ndubuisi Ike",
     twitter: "https://twitter.come/_elvis_ndubuisi",
     github: "https://github.com/elvis_ndubuisi",
+    medium: "",
+    substack: "",
+    website: "",
   },
 };
 
