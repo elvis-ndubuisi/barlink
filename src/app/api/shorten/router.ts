a8a9ad83b1f27d2046170f3b4fef38dc78f3b515
Elvis Ndubuisi Ike <provictor.ie@gmail.com>
2023-12-07 14:59:19 +0100

    chore: defined routes endpoint

---
+++ src/app/api/shorten/router.ts
@@ -0,0 +1,19 @@
+import prisma from "@/db";
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const { longURL } = await req.json();
+  console.log(longURL);
+  //   const short_link = await prisma.url.create({
+  //     data: {
+  //       originalUrl: longURL,
+  //       shortUrl: "https://shorturl",
+  //       shortUrlKey: "asdf",
+  //       expiry: "",
+  //     },
+  //   });
+
+  //   return NextResponse.json({ short_link: short_link.shortUrl });
+  return NextResponse.json({ data: "normal data" });
+}
