65b2b6b5bf685ae4627c159d71ccd166a8256650
elvis-ndubuisi <provictor.ie@gmail.com>
2023-12-16 03:48:03 +0100

    feat: url shorten service

--- src/app/api/gen/shorten/route.ts
+++ src/app/api/gen/shorten/route.ts
@@ -1,15 +1,30 @@
 import prisma from "@/db";
 import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
+import { generateShortkey } from "@/lib/url";
 
-export async function POST(req: Request) {
-  const { longURL } = await req.json();
-  console.log(longURL);
+export async function POST(req: Request, res: Response) {
+  const { longURL, customUrl } = await req.json();
+
+  if (!longURL) return NextResponse.json("Invalid request", { status: 400 });
+
+  const found_link = await prisma.url.findFirst({
+    where: { originalUrl: longURL },
+  });
+
+  if (found_link)
+    return NextResponse.json(
+      { short_link: found_link.shortUrl },
+      { status: 200 }
+    );
+
+  const lks = generateShortkey(customUrl);
+
   const short_link = await prisma.url.create({
     data: {
       originalUrl: longURL,
-      shortUrl: "https://shorturl",
-      shortUrlKey: "asdf",
+      shortUrl: lks.short_link,
+      shortUrlKey: lks.short_key,
     },
   });
 
