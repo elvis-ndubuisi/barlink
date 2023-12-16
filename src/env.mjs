4cce2b7374178e19895831c916291f0b0bd60a6b
elvis-ndubuisi <provictor.ie@gmail.com>
2023-12-16 03:47:27 +0100

    refactor: env values

--- src/env.mjs
+++ src/env.mjs
@@ -4,12 +4,14 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
+    LINK_HOST: z.string().url(),
   },
   client: {
-    NEXT_PUBLIC_GOOGLE_PUBLIC_ID: z.string(),
+    // NEXT_PUBLIC_GOOGLE_PUBLIC_ID: z.string(),
   },
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
-    NEXT_PUBLIC_GOOGLE_ID: process.env.NEXT_PUBLIC_GOOGLE_ID,
+    // NEXT_PUBLIC_GOOGLE_ID: process.env.NEXT_PUBLIC_GOOGLE_ID,
+    LINK_HOST: process.env.LINK_HOST,
   },
 });
