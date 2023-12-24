55f56b950c35007e8cd7fbed3568cb262f7d94f7
Elvis Ndubuisi Ike <provictor.ie@gmail.com>
2023-12-25 00:43:34 +0100

    qrcode page revamp

--- src/app/qrcode/page.tsx
+++ src/app/qrcode/page.tsx
@@ -1,33 +1,21 @@
-import { QrAppEditor } from "@/components/qr-app-editor";
-import qrcode from "qrcode";
-
-const generateQR = async (text: string) => {
-  try {
-    console.log(qrcode.toDataURL(text));
-  } catch (err: any) {
-    console.log(err);
-  }
-};
+import { DesktopGenerator } from "@/components/generators";
 
 export default async function QRCode() {
   // await generateQR("someting");
   return (
     <main className="relative z-0">
-      <section className="dot_bg absolute top-0 left-0 bottom-0 right-0 -z-10" />
+      <section className="dot_bg absolute top-0 left-0 bottom-0 right-0 -z-10 opacity-40 bg-lavender-blue/30" />
 
       <section className="max-w-screen-xl flex flex-col items-center text-center justify-center gap-3 mx-auto px-4 md:px-0 min-h-[20em]">
         <h1 className="font-bold text-4xl max-w-xl">
-          Generate <span className="text-tertiary">dynamic</span> QRcode and
-          custom images for free
+        Create QR Codes That Connect
         </h1>
         <p className="text-lg max-w-xl">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod
-          exercitationem odio porro quisquam facere dolorum praesentium
-          explicabo repellendus! Nisi, aliquid.
+        Generate custom QR codes for any purpose, effortlessly.
         </p>
       </section>
 
-      <QrAppEditor />
+      <DesktopGenerator/>
     </main>
   );
 }
