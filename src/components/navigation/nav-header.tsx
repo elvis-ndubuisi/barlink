81b3297f6cd4a5c1b0ea1ae1bc1c3245d07510d2
Elvis Ndubuisi Ike <provictor.ie@gmail.com>
2024-01-29 18:41:52 +0100

    fix: switched components imports

--- src/components/navigation/nav-header.tsx
+++ src/components/navigation/nav-header.tsx
@@ -1,67 +1,71 @@
-import { Button } from "@nextui-org/button";
-import { UserMenu } from "./user-menu";
-import { Link } from "@nextui-org/link";
-import { Chip } from "@nextui-org/chip";
-import { Icons } from "../icons";
+// import { Button } from "@nextui-org/button";
+// import { UserMenu } from "./user-menu";
+// import { Link } from "@nextui-org/link";
+// import { Chip } from "@nextui-org/chip";
+// import { Icons } from "../icons";
+
+// export function NavHeader() {
+//   let user = null;
+//   return (
+//     <header className="pt-3 pb-2 sticky top-0 w-full z-20 h-14 backdrop-blur-md bg-deep-sapphire-950 text-white">
+//       <section className="flex items-center justify-between mx-auto max-w-screen-xl px-4 md:px-0">
+//         <div className="flex items-center gap-2">
+//           <Icons.qrcode />
+//           <h1 className="font-bold text-2xl">Barlink</h1>
+//           <Chip size="sm" className="text-dark bg-mid-light">
+//             v2.0.0
+//           </Chip>
+//         </div>
+//         <nav className="flex items-center gap-6">
+//           <ul className="inline-flex items-center gap-3 font-medium">
+//             <li>
+//               <Link
+//                 className="font-medium text-sm hover:underline underline-offset-4 text-dark hover:text-cinnabar"
+//                 href={"/"}
+//               >
+//                 Home
+//               </Link>
+//             </li>
+//             <li>
+//               <Link
+//                 className="font-medium text-sm hover:underline underline-offset-4 text-dark hover:text-cinnabar"
+//                 href={"/shorten"}
+//               >
+//                 Shorten
+//               </Link>
+//             </li>
+//             <li>
+//               <Link
+//                 className="font-medium text-sm hover:underline underline-offset-4 text-dark hover:text-cinnabar"
+//                 href={"/gen/qrcode"}
+//               >
+//                 QRcode
+//               </Link>
+//             </li>
+//             <li>
+//               <Link
+//                 className="font-medium text-sm hover:underline underline-offset-4 text-dark hover:text-cinnabar"
+//                 // href={"/integration"}
+//                 href={"#"}
+//               >
+//                 Integration
+//               </Link>
+//             </li>
+//           </ul>
+//           {/* {user ? <UserMenu /> : <Button size="sm">Sign In</Button>} */}
+//           <Button
+//             size="md"
+//             radius="none"
+//             className="bg-dark text-light rounded-md"
+//           >
+//             Get started
+//           </Button>
+//         </nav>
+//       </section>
+//     </header>
+//   );
+// }
 
 export function NavHeader() {
-  let user = null;
-  return (
-    <header className="pt-3 pb-2 sticky top-0 w-full z-20 h-14 backdrop-blur-md bg-deep-sapphire-950 text-white">
-      <section className="flex items-center justify-between mx-auto max-w-screen-xl px-4 md:px-0">
-        <div className="flex items-center gap-2">
-          <Icons.qrcode />
-          <h1 className="font-bold text-2xl">Barlink</h1>
-          <Chip size="sm" className="text-dark bg-mid-light">
-            v2.0.0
-          </Chip>
-        </div>
-        <nav className="flex items-center gap-6">
-          <ul className="inline-flex items-center gap-3 font-medium">
-            <li>
-              <Link
-                className="font-medium text-sm hover:underline underline-offset-4 text-dark hover:text-cinnabar"
-                href={"/"}
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="font-medium text-sm hover:underline underline-offset-4 text-dark hover:text-cinnabar"
-                href={"/shorten"}
-              >
-                Shorten
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="font-medium text-sm hover:underline underline-offset-4 text-dark hover:text-cinnabar"
-                href={"/gen/qrcode"}
-              >
-                QRcode
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="font-medium text-sm hover:underline underline-offset-4 text-dark hover:text-cinnabar"
-                // href={"/integration"}
-                href={"#"}
-              >
-                Integration
-              </Link>
-            </li>
-          </ul>
-          {/* {user ? <UserMenu /> : <Button size="sm">Sign In</Button>} */}
-          <Button
-            size="md"
-            radius="none"
-            className="bg-dark text-light rounded-md"
-          >
-            Get started
-          </Button>
-        </nav>
-      </section>
-    </header>
-  );
+	return <header>header</header>;
 }
--- src/components/navigation/user-menu.tsx
+++ src/components/navigation/user-menu.tsx
@@ -1,35 +1,39 @@
-"use client";
+// "use client";
 
-import {
-  Dropdown,
-  DropdownTrigger,
-  DropdownMenu,
-  DropdownItem,
-} from "@nextui-org/dropdown";
-import { User } from "@nextui-org/user";
+// import {
+//   Dropdown,
+//   DropdownTrigger,
+//   DropdownMenu,
+//   DropdownItem,
+// } from "@nextui-org/dropdown";
+// import { User } from "@nextui-org/user";
+
+// export function UserMenu() {
+//   return (
+//     <Dropdown className="bg-bg border border-main">
+//       <DropdownTrigger>
+//         <User
+//           className="hover:cursor-pointer"
+//           name="Elvis ndubuisi"
+//           description="non yet"
+//           avatarProps={{
+//             src: "https://i.pravatar.cc/150?u=a04258114e29026702d",
+//           }}
+//         />
+//       </DropdownTrigger>
+
+//       <DropdownMenu aria-label="Static Actions">
+//         <DropdownItem key="new">New file</DropdownItem>
+//         <DropdownItem key="copy">Copy link</DropdownItem>
+//         <DropdownItem key="edit">Edit file</DropdownItem>
+//         <DropdownItem key="delete" className="text-danger" color="danger">
+//           Delete file
+//         </DropdownItem>
+//       </DropdownMenu>
+//     </Dropdown>
+//   );
+// }
 
 export function UserMenu() {
-  return (
-    <Dropdown className="bg-bg border border-main">
-      <DropdownTrigger>
-        <User
-          className="hover:cursor-pointer"
-          name="Elvis ndubuisi"
-          description="non yet"
-          avatarProps={{
-            src: "https://i.pravatar.cc/150?u=a04258114e29026702d",
-          }}
-        />
-      </DropdownTrigger>
-
-      <DropdownMenu aria-label="Static Actions">
-        <DropdownItem key="new">New file</DropdownItem>
-        <DropdownItem key="copy">Copy link</DropdownItem>
-        <DropdownItem key="edit">Edit file</DropdownItem>
-        <DropdownItem key="delete" className="text-danger" color="danger">
-          Delete file
-        </DropdownItem>
-      </DropdownMenu>
-    </Dropdown>
-  );
+	return <div></div>;
 }
