baae272adf952ac9edceced87116559a2656b784
elvis-ndubuisi <provictor.ie@gmail.com>
2024-02-13 02:07:20 +0100

    feat: added shadcn component setup

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "@/components/ui/button";
 
 export const Route = createFileRoute("/")({
 	component: Index,
@@ -7,6 +8,7 @@ export const Route = createFileRoute("/")({
 function Index() {
 	return (
 		<div className='p-2'>
+			<Button>Click Me!</Button>
 			<h3>Welcome Home!</h3>
 		</div>
 	);
