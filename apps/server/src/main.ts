ad8cd1ab5a11f4c88bdb83cae50bac9cf61a253d
elvis-ndubuisi <provictor.ie@gmail.com>
2024-03-04 14:49:00 +0100

    refactor: remove auth route controller

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { VersioningType } from "@nestjs/common";
+import helmet from "helmet";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	app.enableCors();
 	app.enableVersioning({ type: VersioningType.URI });
+	app.enableCors();
+	app.use(helmet());
 	await app.listen(3000);
 }
 bootstrap();
