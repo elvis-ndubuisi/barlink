39dd25b4a3a8c817ea0139e96b0546893dde3b7b
elvis-ndubuisi <provictor.ie@gmail.com>
2024-03-04 16:21:35 +0100

    feat: add module for prisma client

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from "./app.service";
 import { QrcodesModule } from "./qrcodes/qrcodes.module";
 import { AuthModule } from "./auth/auth.module";
 import { UsersModule } from "./users/users.module";
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-	imports: [QrcodesModule, AuthModule, UsersModule],
+	imports: [QrcodesModule, AuthModule, UsersModule, DatabaseModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
