7abc4c7a369564ae4cc0aa6f0f7076dcd8f1b3e7
elvis-ndubuisi <provictor.ie@gmail.com>
2024-03-04 14:59:02 +0100

    feat: define route controllers

---
+++ apps/server/src/qrcodes/qrcodes.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { QrcodesService } from './qrcodes.service';
+import { CreateQrcodeDto } from './dto/create-qrcode.dto';
+import { UpdateQrcodeDto } from './dto/update-qrcode.dto';
+
+@Controller('qrcodes')
+export class QrcodesController {
+  constructor(private readonly qrcodesService: QrcodesService) {}
+
+  @Post()
+  create(@Body() createQrcodeDto: CreateQrcodeDto) {
+    return this.qrcodesService.create(createQrcodeDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.qrcodesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.qrcodesService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateQrcodeDto: UpdateQrcodeDto) {
+    return this.qrcodesService.update(+id, updateQrcodeDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.qrcodesService.remove(+id);
+  }
+}
---
+++ apps/server/src/qrcodes/static/static.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { StaticService } from './static.service';
+import { CreateStaticDto } from './dto/create-static.dto';
+import { UpdateStaticDto } from './dto/update-static.dto';
+
+@Controller('static')
+export class StaticController {
+  constructor(private readonly staticService: StaticService) {}
+
+  @Post()
+  create(@Body() createStaticDto: CreateStaticDto) {
+    return this.staticService.create(createStaticDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.staticService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.staticService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateStaticDto: UpdateStaticDto) {
+    return this.staticService.update(+id, updateStaticDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.staticService.remove(+id);
+  }
+}
