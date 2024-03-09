465394884dbab9850994ce8dde94d7fe02ea22e8
elvis-ndubuisi <provictor.ie@gmail.com>
2024-03-09 08:37:12 +0100

    refactor: group nest project to api

---
+++ apps/api/src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { QrcodesModule } from "./qrcodes/qrcodes.module";
+import { AuthModule } from "./auth/auth.module";
+import { UsersModule } from "./users/users.module";
+import { DatabaseModule } from "@package/database";
+
+@Module({
+	imports: [QrcodesModule, AuthModule, UsersModule, DatabaseModule],
+	controllers: [AppController],
+	providers: [AppService],
+})
+export class AppModule {}
