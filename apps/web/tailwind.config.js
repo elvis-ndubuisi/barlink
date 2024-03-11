179711c952313c8ec7ab04a96f3ffad4e53c53f6
elvis-ndubuisi <provictor.ie@gmail.com>
2024-03-11 16:44:54 +0100

    style: add custom font to web

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["General Sans", ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	plugins: [],
+};
