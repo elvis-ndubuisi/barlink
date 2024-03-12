cf6284112cfa8d2e53595b6bc16bb65b42edd13a
elvis-ndubuisi <provictor.ie@gmail.com>
2024-03-12 18:23:50 +0100

    refactor: base-header display

--- apps/web/src/components/base-header.tsx
+++ apps/web/src/components/base-header.tsx
@@ -1,7 +1,34 @@
 import { Link } from "@tanstack/react-router";
-import { Button, Group } from "@mantine/core";
+import { Button, Group, Burger } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import ThemeSwitcgh from "./theme-switch";
+
+const links = [
+	{
+		link: "/generate",
+		label: "Generate",
+	},
+	{
+		link: "/shorten",
+		label: "Shorten",
+	},
+	{
+		link: "/faq",
+		label: "FAQ",
+	},
+	{
+		link: "/pricing",
+		label: "Pricing",
+	},
+	{
+		link: "/integration",
+		label: "Integration",
+	},
+];
 
 export default function BaseHeader() {
+	const [opened, { toggle }] = useDisclosure(false);
+
 	return (
 		<header className=''>
 			<section className='mx-auto flex max-w-screen-xl items-center justify-between px-2 py-4 md:px-1'>
@@ -13,29 +40,43 @@ export default function BaseHeader() {
 						src=''
 						alt=''
 					/>
-					<span className='text-[--mantine-color-brand-8]'>Barlink</span>
+					<span className=''>Barlink</span>
 				</Link>
 
-				<nav role='navigation' className='flex items-center gap-2 text-sm font-medium'>
-					<Link to='/'>Use Cases</Link>
-					<Link to='/'>Features</Link>
-					<Link to='/'>FAQ</Link>
-					<Link to='/'>Integration</Link>
-					<Link to='/'>Pricing</Link>
-				</nav>
+				<Group gap={5} visibleFrom='xs'>
+					{links.map((link) => (
+						<Link
+							to={link.link}
+							key={link.label}
+							className='rounded-[--mantine-radius-sm] px-1.5 py-0.5 hover:text-[--mantine-color-brand-6]'
+							activeProps={{
+								className: "bg-[--mantine-color-brand-8] text-white hover:text-white",
+							}}>
+							{link.label}
+						</Link>
+					))}
+				</Group>
 
-				<Group justify='center'>
+				<Group justify='center' gap={"xs"} visibleFrom='xs'>
 					<Link to='/login'>
 						<Button variant='light'>Log in</Button>
 					</Link>
 					<Link to='/signup'>
 						<Button
 							variant='filled'
-							gradient={{ from: "orange.7", to: "orange.5", deg: 90 }}>
+							gradient={{
+								from: "orange.7",
+								to: "orange.5",
+								deg: 90,
+							}}>
 							Try for free
 						</Button>
 					</Link>
+					<ThemeSwitcgh />
 				</Group>
+
+				<Burger opened={opened} onClick={toggle} hiddenFrom='xs' size='sm' />
+				{/* TODO: Add nav menu */}
 			</section>
 		</header>
 	);
