9334b36a95624a13cfede3f7b3d854b7898f1271
elvis-ndubuisi <provictor.ie@gmail.com>
2024-03-14 19:14:44 +0100

    feat: add new background to snippets

--- apps/web/src/components/background-snippets.tsx
+++ apps/web/src/components/background-snippets.tsx
@@ -15,3 +15,12 @@ export function GradientDownBackground() {
 		<div className='absolute top-0 z-[-2] h-screen w-screen bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]'></div>
 	);
 }
+
+export function SideGlowBackground() {
+	return (
+		<div className='relative h-full w-full bg-slate-950'>
+			<div className='absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]'></div>
+			<div className='absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]'></div>
+		</div>
+	);
+}
