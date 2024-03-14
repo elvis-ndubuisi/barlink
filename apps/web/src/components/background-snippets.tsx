fe72bebb3ead7a94bcbfe028fc460e4a39350b9e
elvis-ndubuisi <provictor.ie@gmail.com>
2024-03-14 15:57:03 +0100

    feat: add custom html background

---
+++ apps/web/src/components/background-snippets.tsx
@@ -0,0 +1,17 @@
+export function TallGridBackground() {
+	return (
+		<div className='absolute bottom-0 left-0 right-0 top-0 -z-10 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]' />
+	);
+}
+
+export function TopLightBackground() {
+	return (
+		<div className='absolute top-0 -z-10 h-screen w-screen bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]' />
+	);
+}
+
+export function GradientDownBackground() {
+	return (
+		<div className='absolute top-0 z-[-2] h-screen w-screen bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]'></div>
+	);
+}
